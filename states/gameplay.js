5ea2393c41da9c383d714ec38911f179b41b7f92
jaykapasi28 <jaykapasi2000@gmail.com>
2022-03-01 12:23:50 +0530

    game validation

--- states/gameplay.js
+++ states/gameplay.js
@@ -1,4 +1,3 @@
-
 export default class GameplayState extends Phaser.State {
     init() {
 
@@ -15,72 +14,88 @@ export default class GameplayState extends Phaser.State {
         this.d1 = 106.5;
         this.board = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "board");
         this.board.anchor.setTo(0.5, 0.5);
-        this.board.inputEnabled = true;
-        this.board.events.onInputDown.add(this.clickedOnHole, this);
         this.isCoinBlue = true;
 
-        this.arr = []
-
-        this.k  = 0;
-        for(let i = 0; i <= 5; i++) {
-            for(let j = 0; j <= 6; j++) {
-                this.k++
-                this.arr.push([i, j, {coin: null}]);   
+        this.arr = [];
+        this.k = 0;
+        for (let i = 0; i <= 5; i++) {
+            this.arr.push([]);
+            for (let j = 0; j <= 6; j++) {
+                this.arr[i].push({ row: i, column: j, coin: null });
             }
         }
+        console.log(this.arr);
 
-        var k = 0;
         for(let i=0; i<7; i++) {
             var graphics = game.add.graphics(0, 0);
-            graphics.beginFill(0xFF0000, 0);
-            // graphics.drawRect(140, 166.5, 85, 667);
+            graphics.beginFill(0x34abeb, 0);
             graphics.drawRect(140 + 106 * i, 166.5, 85, 667);
             graphics.endFill();
             graphics.inputEnabled = true;
             graphics.events.onInputDown.add(this.clickedOnColumn, this);
-            graphics.id = k;
-            k++;
+            graphics.id = this.k;
+            
+            this.k++;
         }
     }
 
-    clickedOnColumn(graphic) {
-        // console.log(this.arr);
-        // console.log(graphic.id);
+    clickedOnColumn(g) {
         for(let i = 5; i >= 0; i--){
-                if(this.arr[0, 1, 2] == i, graphic.id, {coin: null}) {
-                    if(this.isCoinBlue){ 
-                        this.blueCoin = this.game.add.sprite(this.w + this.d + this.d1 * graphic.id, this.h + this.d + this.d1 * i, "blue-coin");
-                        this.blueCoin.anchor.setTo(0.5, 0.5);
-                        this.isCoinBlue = false;
-                        break;
-                    } else if(!this.isCoinBlue) {
-                        this.blueCoin = this.game.add.sprite(this.w + this.d + this.d1 * graphic.id, this.h + this.d + this.d1 * i, "red-coin");
-                        this.blueCoin.anchor.setTo(0.5, 0.5);
-                        this.isCoinBlue = true;
-                        break;
-                    }
+            if(this.arr[i][g.id].coin == null) {
+                if(this.isCoinBlue) {
+                    this.arr[i][g.id].coin = "blue";
+                    this.colorCoin = this.game.add.sprite(this.w + this.d + this.d1 * g.id, this.h + this.d + this.d1 * i, "blue-coin");
+                    this.colorCoin.anchor.setTo(0.5, 0.5);
+                    this.isCoinBlue = false;
+                    break;
+                } else if(!this.isCoinBlue) {
+                    this.arr[i][g.id].coin = "red";
+                    this.colorCoin = this.game.add.sprite(this.w + this.d + this.d1 * g.id, this.h + this.d + this.d1 * i, "red-coin");
+                    this.colorCoin.anchor.setTo(0.5, 0.5);
+                    this.isCoinBlue = true;
+                    break;
                 }
             }
-            console.log(this.isCoinBlue);
-
-
-
-        // for(let i = 5; i >= 0; i--){
-        //     for(let j = 0; j < this.arr.length; j++) {
-        //         if(this.arr[j][0] == i && this.arr[j][1] == graphic.id && this.arr[j][2].coin == null) {
-        //             console.log(this.arr[j]);
-        //             this.blueCoin = this.game.add.sprite(this.w + this.d + this.d1 * graphic.id, this.h + this.d + this.d1 * i, "blue-coin");
-        //             this.blueCoin.anchor.setTo(0.5, 0.5);
-        //             break;
-        //         }
-        //         break;
-        //     }
-        //     break;
-        // }
+        }
+        this.checkForWinner();
     }
 
-    clickedOnHole(board) {
-        
+    checkForWinner() {
+        for (let i = 0; i <= 5; i++) {
+            for (let j = 0; j <= 6; j++) {
+                if(this.arr[i][j].coin === "blue" && this.arr[i][j+1].coin === "blue" && this.arr[i][j+2].coin === "blue" && this.arr[i][j+3].coin === "blue" ) {
+                    alert("Blue Wins!")
+                }
+
+                if(this.arr[i][j].coin === "red" && this.arr[i][j+1].coin === "red" && this.arr[i][j+2].coin === "red" && this.arr[i][j+3].coin === "red" ) {
+                    alert("red Wins!")
+                }
+
+                if(this.arr[i][j].coin === "blue" && this.arr[i][j-1].coin === "blue" && this.arr[i][j-2].coin === "blue" && this.arr[i][j-3].coin === "blue" ) {
+                    alert("Blue Wins!")
+                }
+
+                if(this.arr[i][j].coin === "red" && this.arr[i][j-1].coin === "red" && this.arr[i][j-2].coin === "red" && this.arr[i][j-3].coin === "red" ) {
+                    alert("red Wins!")
+                }
+
+                if(this.arr[i][j].coin === "blue" && this.arr[i+1][j].coin === "blue" && this.arr[i+2][j].coin === "blue" && this.arr[i+3][j].coin === "blue" ) {
+                    alert("Blue Wins!")
+                }
+
+                if(this.arr[i][j].coin === "red" && this.arr[i+1][j].coin === "red" && this.arr[i+2][j].coin === "red" && this.arr[i+3][j].coin === "red" ) {
+                    alert("red Wins!")
+                }
+
+                if(this.arr[i][j].coin === "blue" && this.arr[i-1][j].coin === "blue" && this.arr[i-2][j].coin === "blue" && this.arr[i-3][j].coin === "blue" ) {
+                    alert("Blue Wins!")
+                }
+
+                if(this.arr[i][j].coin === "red" && this.arr[i-1][j].coin === "red" && this.arr[i-2][j].coin === "red" && this.arr[i-3][j].coin === "red" ) {
+                    alert("red Wins!")
+                }
+            }
+        }
     }
 
     update() {
